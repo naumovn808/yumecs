d31179d2d6bf7dc7b83bd64011186959d390dca8
naumovn808 <naumovn808@gmail.com>
2024-10-25 18:32:02 +0500

    feat: add tg back history btn

--- src/js/utils/go-back.js
+++ src/js/utils/go-back.js
@@ -4,7 +4,28 @@ export function goBack() {
 	if (!btn) return;
 
 	btn.addEventListener("click", function () {
-
 		window.history.back();
 	});
+
+	if (typeof Telegram !== "undefined" && Telegram.WebApp) {
+		btn.addEventListener("click", () => {
+
+			Telegram.WebApp.BackButton.onClick(() => {
+				handleBack();
+			});
+
+			Telegram.WebApp.BackButton.show();
+			Telegram.WebApp.BackButton.click();
+		});
+
+		function handleBack() {
+			if (window.location.search && window.location.pathname !== '/') {
+				history.back();
+			} else {
+				location.href = "/";
+			}
+		}
+	} else {
+		console.warn("Telegram WebApp API недоступен.");
+	}
 }
