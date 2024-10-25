1fcc0cf8a194e062f6e39552870e1b1984db8bd3
naumovn808 <naumovn808@gmail.com>
2024-10-25 18:05:54 +0500

    feat: add accordeon

--- src/js/utils/tabs.js
+++ src/js/utils/tabs.js
@@ -1,25 +1,30 @@
 export function tabs() {
-	const tabsLinks = document.querySelectorAll('.tabs__link');
-	const tabsContent = document.querySelectorAll('[data-tab-content]');
-	const bg = document.querySelector('.tabs__bg');
+	const tabsLinks = document.querySelectorAll(".tabs__link");
+	const tabsContent = document.querySelectorAll("[data-tab-content]");
+	const bg = document.querySelector(".tabs__bg");
+	const tabsAbout = document.querySelector(".tabs__about-head");
 
-	if (!tabsLinks || !tabsContent || !bg) return;
+	if (!tabsLinks || !tabsContent || !bg || !tabsAbout) return;
 
 	tabsLinks.forEach((link, index) => {
-		link.addEventListener('click', () => {
+		link.addEventListener("click", () => {
+			tabsLinks.forEach((btn) => btn.classList.remove("active"));
+			tabsContent.forEach((tab) => tab.classList.remove("active"));
 
-			tabsLinks.forEach(btn => btn.classList.remove('active'));
-			tabsContent.forEach(tab => tab.classList.remove('active'));
-
-			link.classList.add('active');
+			link.classList.add("active");
 
 			const targetTab = document.querySelector(`[data-tab-content="${link.dataset.tab}"]`);
 
 			if (targetTab) {
-				targetTab.classList.add('active');
+				targetTab.classList.add("active");
 			}
 
 			bg.style.transform = `translateX(${index * 100}%)`;
 		});
 	});
-};
+
+	tabsAbout.addEventListener("click", (e) => {
+		e.preventDefault();
+		tabsAbout.classList.toggle("active");
+	});
+}
