0647a8550dc283ba44056e1e3492369fb997935a
naumovn808 <naumovn808@gmail.com>
2024-10-31 11:15:48 +0500

    fix: fix menu links

--- src/js/helpers/scroll-links.js
+++ src/js/helpers/scroll-links.js
@@ -7,6 +7,20 @@ export function scrollLinks() {
 		centeredSlides: false,
 		loop: false,
 		grabCursor: true,
+		watchSlidesProgress: true,
+		on: {
+			init: function () {
+			  // Находим слайд с классом active
+			  const activeSlide = Array.from(this.slides).findIndex(slide =>
+				slide.classList.contains('active')
+			  );
+
+			  // Если найден active-слайд, перемещаем его в левую часть экрана
+			  if (activeSlide !== -1) {
+				this.slideTo(activeSlide, 0); // Сдвигаем слайдер к активному слайду
+			  }
+			},
+		  },
 	});
 
 }
